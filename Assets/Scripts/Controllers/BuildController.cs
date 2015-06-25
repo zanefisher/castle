615231a629b87260ef25500d1f6e456169424b6d
Will Jeffers <wfj205@gmail.com>
2015-06-25 17:27:54 -0400

    Combined the wall building from Oliver's and my scenes. Took functions from his SpawnWalls script and put them into my BuildController.

--- Assets/Scripts/Controllers/BuildController.cs
+++ Assets/Scripts/Controllers/BuildController.cs
@@ -34,6 +34,8 @@ public class BuildController : MonoBehaviour {
 				endTower = Instantiate (endTowerPrefab, mousePos, Quaternion.identity) as GameObject;
 			}else if(Input.GetMouseButtonUp (0)){
 				endTower.transform.position = startTower.transform.position + distance * startTower.transform.forward;
+				Destroy (newWall);
+				StartCoroutine (BuildWall (startTower.transform.position, endTower.transform.position));
 				buildingWalls = false;
 			}else if(Input.GetMouseButton (0)){
 				//end.transform.position = mousePos;
@@ -72,5 +74,101 @@ public class BuildController : MonoBehaviour {
 			rayPos = hitDist.point;
 		}
 		mousePos = new Vector3 (rayPos.x, 0f, rayPos.z);
+	}
+
+	public float wallSegments;
+	public float stepSize = 1f;
+	public float stepDelay=1f;
+	public float wallOffsetHeight=2f;
+	public float buildSpeed=1f;
+	public GameObject wallChunk;
+	//public GameObject tower;
+	//public Transform gunObj;
+	GameObject prevWall;
+	IEnumerator BuildWall(Vector3 startPos, Vector3 endPos){
+		
+		Vector3 buildDirection=endPos-startPos;
+		Vector3 currentBuildPos=startPos;
+		Vector3 lastWallPos=startPos;
+		
+		
+		wallSegments=Vector3.Distance(startPos,endPos)/stepSize;
+		int i=0;
+		while(i<=wallSegments){
+			
+			Ray theRay;
+			RaycastHit rayHit;
+			GameObject wall =Instantiate(wallChunk,currentBuildPos+Vector3.up*5f,Quaternion.LookRotation(buildDirection)) as GameObject;
+			
+			if (Physics.Raycast(wall.transform.position, Vector3.down, out rayHit) || Physics.Raycast(wall.transform.position, Vector3.up, out rayHit)){
+				Debug.Log("hit");
+				
+				wall.transform.position=new Vector3(rayHit.point.x,rayHit.point.y+wallOffsetHeight,rayHit.point.z);
+				
+				wall.transform.rotation = Quaternion.LookRotation((lastWallPos-wall.transform.position),rayHit.normal);
+				
+				
+				//wall.GetComponent<BuildEffect>().Build(wallOffsetHeight);
+				
+				//wall.transform.rotation=Quaternion.FromToRotation(Vector3.up, rayHit.normal);
+			}
+			
+			Vector3 wallScale=wall.transform.localScale;
+			wallScale.z=stepSize;
+			wall.transform.localScale=wallScale;
+			
+			if(prevWall!=null){
+				ReshapeCubes(wall,prevWall);
+			}
+			
+			currentBuildPos+=buildDirection.normalized*stepSize;
+			lastWallPos=wall.transform.position;
+			prevWall=wall;
+			
+			i++;
+			yield return new WaitForSeconds(stepDelay);
+		}
+		prevWall=null;
+		yield break;
+	}
+
+	void ReshapeCubes(GameObject oldCube, GameObject newCube){
+		//oldCube.GetComponent<MeshRenderer>().material.color=Color.red;
+		//newCube.GetComponent<MeshRenderer>().material.color=Color.blue;
+		//		Debug.Log("called");
+		Mesh newCubeMesh = newCube.GetComponent<MeshFilter> ().mesh;
+		Mesh oldCubeMesh = oldCube.GetComponent<MeshFilter> ().mesh;
+		
+		Vector3[] newCubeVerts = new Vector3[newCubeMesh.vertexCount];
+		newCubeVerts = newCubeMesh.vertices;
+		Vector3[] oldCubeVerts = new Vector3[oldCubeMesh.vertexCount]; 
+		oldCubeVerts = oldCubeMesh.vertices;
+
+		for (int i=0; i<=23; i++) {
+			if (i == 4 || i == 10 || i == 23) {
+				newCubeVerts [i] = newCube.transform.InverseTransformPoint (oldCube.transform.TransformPoint (oldCubeVerts [2]));
+				//newCubeVerts[i]=transform.InverseTransformPoint(Vector3.Lerp(transform.TransformPoint(newCubeVerts[i]),transform.TransformPoint(oldCubeVerts[2]),.5f));
+			} else if (i == 5 || i == 11 || i == 17) {
+				newCubeVerts [i] = newCube.transform.InverseTransformPoint (oldCube.transform.TransformPoint (oldCubeVerts [3]));
+				//newCubeVerts[i]=transform.InverseTransformPoint(Vector3.Lerp(transform.TransformPoint(newCubeVerts[i]),transform.TransformPoint(oldCubeVerts[3]),.5f));
+				//newCubeVerts[i]=Vector3.Lerp(newCubeVerts[i],oldCubeVerts[3],.5f);
+			} else if (i == 6 || i == 12 || i == 20) {
+				newCubeVerts [i] = newCube.transform.InverseTransformPoint (oldCube.transform.TransformPoint (oldCubeVerts [0]));
+				//newCubeVerts[i]=transform.InverseTransformPoint(Vector3.Lerp(transform.TransformPoint(newCubeVerts[i]),transform.TransformPoint(oldCubeVerts[0]),.5f));
+				//newCubeVerts[i]=Vector3.Lerp(newCubeVerts[i],oldCubeVerts[0],.5f);
+			} else if (i == 7 || i == 14 || i == 18) {
+				newCubeVerts [i] = newCube.transform.InverseTransformPoint (oldCube.transform.TransformPoint (oldCubeVerts [1]));
+				//newCubeVerts[i]=transform.InverseTransformPoint(Vector3.Lerp(transform.TransformPoint(newCubeVerts[i]),transform.TransformPoint(oldCubeVerts[1]),.5f));
+				//newCubeVerts[i]=Vector3.Lerp(newCubeVerts[i],oldCubeVerts[1],.5f);
+			}
+			
+			
+		}
+
+		newCubeMesh.vertices=newCubeVerts;
+		oldCubeMesh.vertices=oldCubeVerts;
+		
+		newCubeMesh.RecalculateBounds();
+		oldCubeMesh.RecalculateBounds();
 	}
 }
